bd0853c55f89b844193a46abfbfe5197f529ed6c
jmoore1818 <Justindonaldmoore@gmail.com>
2016-08-02 14:31:38 -0400

    First-Assignment

---
+++ HI.java
@@ -0,0 +1,63 @@
+package com.hi;
+
+import java.util.Scanner;
+
+public class HI {
+
+	public static void main(String[] args) {
+	
+		String greeting1 = "Hello World";
+			System.out.println(greeting1);
+		
+		int greeting2 = 1492;
+		System.out.println("Columbus Sailed the world in " + greeting2);
+		
+		boolean dogAtHome = true; 
+			System.out.println("It is " + dogAtHome + " that I have a dog at home");
+	
+		String todaysDate = "8/2/2016";
+			System.out.println("Today is " + todaysDate);
+		
+		double amountOfMoney = 5.45;
+		System.out.println("I spent " + amountOfMoney + " on a cheesburger");
+		
+		String singleLetter = "X";
+		System.out.println(singleLetter + " marks the spot");
+		
+		String[]movieFavs = {"Forest Gump","Jurassic Park","Cinderella","Snow White", "Lion King", "Tangled", "Finding Nemo", "Toy Story", "Beauty and the Beast", "Frozen"};
+		System.out.println("My all-time favorite movie is " + movieFavs[0]);
+		System.out.println("My second favorite movie is " + movieFavs[1]);
+	}
+	public boolean alienEggs(){
+		if (eggs = 4) System.out.println("Yummy");
+	}
+	protected int birdEggs(){
+		System.out.println("Bird eggs " + birdEggs);
+	}
+	public int retail(){
+		cost + profit = totalTranscations
+	}
+	private String favoriteWord(){
+		System.out.println("My favorite word is " + favoriteWord);
+	}
+	public boolean doYouOwnADog(){
+		if (doYouOwnADog(true))
+				System.out.println("You're lucky!");		
+	}
+	private int animalsAtShelter(){
+		cats + dog = totalAnimals
+	}
+	protected double tollsCost(){
+		marylandCost + delawareCost = totalCost
+	}
+	protected boolean doYouDriveMoreThan50Miles(){
+		if (doYouDriveMoreThan50Miles(true))
+			System.out.println("Make sure you get gas");
+	}
+	public String worstHockeyTeam(){
+		System.out.println("The worst team " + worstHockeyTeam);
+	}
+	protected int studentsInClass(){
+		studentsPresent + studentsAbsent = total
+	}
+}
